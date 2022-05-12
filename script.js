5ced05f89272ca31febe6151bed4fe1be794e773
MariusDevelops <brazasmarius@gmail.com>
2022-05-12 13:20:20 +0200

    feat: input to change number of squares + black hover over them

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 /* Create a webpage with a 16x16 grid of square divs. */
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = `repeat(16 , 1fr)`;
-board.style.gridTemplateRows = `repeat(16 , 1fr)`;
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove()); // remove empty squares
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "blue";
-  board.insertAdjacentElement("beforeend", square);
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    /* Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would. */
+    square.addEventListener(
+      "mouseover",
+      () => (square.style.backgroundColor = "black")
+    );
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
 
-/* Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would. */
+populateBoard(16);
 
-/* Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. */
+/* Add a button that will change number of squares per side for the new grid. */
+function changeSize(input) {
+  populateBoard(input);
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("too many squares");
+  }
+}
